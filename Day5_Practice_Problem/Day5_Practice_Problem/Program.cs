9fbfef75bc274ba13d0d84e4905fbc2d325d181a
virendra <virendrachouhan1494@gmail.com>
2022-07-31 01:19:59 +0530

    Flip_Implimented

--- Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
+++ Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
@@ -6,7 +6,41 @@ namespace Day5_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Head = 1;
+            int Tail = 0;
+            int headcount = 0;
+            int tailcount = 0;
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+
+
+                Console.WriteLine("Flip Coin");
+                
+                Random random = new Random();
+                int flip = random.Next(0, 2);
+                Console.WriteLine(flip);
+
+                if (flip == Head)
+                {
+                    headcount++;
+                }
+                else
+                {
+                    tailcount++;
+                }
+
+                
+            }
+            int Head_Percentage = (headcount * 100) / num;
+            int Tail_Percentage = (tailcount * 100) / num;
+            Console.WriteLine("Head_Percentage" + Head_Percentage);
+            Console.WriteLine("Tail_Percentage" + Tail_Percentage);
+            Console.WriteLine("how much time Head came" + headcount);
+            Console.WriteLine("how much time Tail came" + tailcount);
         }
+
+
+
     }
 }
