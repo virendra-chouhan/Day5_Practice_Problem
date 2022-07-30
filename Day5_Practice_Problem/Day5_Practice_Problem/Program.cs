788cf1a6c2b280d8b51d2e6492ed89fe2ceb1b26
virendra <virendrachouhan1494@gmail.com>
2022-07-31 01:34:23 +0530

    Poerof2

--- Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
+++ Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
@@ -4,43 +4,30 @@ namespace Day5_Practice_Problem
 {
     class Program
     {
+        static bool isPowerOfTwo(int n)
+        {
+            if (n == 0)
+                return false;
+
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    return false;
+
+                n = n / 2;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            int Head = 1;
-            int Tail = 0;
-            int headcount = 0;
-            int tailcount = 0;
-            int num = int.Parse(Console.ReadLine());
-            for (int i = 1; i < num; i++)
-            {
+           
 
-
-                Console.WriteLine("Flip Coin");
-                
-                Random random = new Random();
-                int flip = random.Next(0, 2);
-                Console.WriteLine(flip);
-
-                if (flip == Head)
-                {
-                    headcount++;
-                }
-                else
-                {
-                    tailcount++;
-                }
-
-                
-            }
-            int Head_Percentage = (headcount * 100) / num;
-            int Tail_Percentage = (tailcount * 100) / num;
-            Console.WriteLine("Head_Percentage" + Head_Percentage);
-            Console.WriteLine("Tail_Percentage" + Tail_Percentage);
-            Console.WriteLine("how much time Head came" + headcount);
-            Console.WriteLine("how much time Tail came" + tailcount);
+            // Driver program
+            
+                Console.WriteLine(isPowerOfTwo(31) ? "Yes" : "No");
+                Console.WriteLine(isPowerOfTwo(64) ? "Yes" : "No");
+            
         }
-
-
 
     }
 }
