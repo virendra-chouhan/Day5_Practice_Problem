6f8f82f286ec26e188b905cb20187d1b1f3b2900
virendra <virendrachouhan1494@gmail.com>
2022-08-01 16:59:34 +0530

    Harmonic

--- Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
+++ Day5_Practice_Problem/Day5_Practice_Problem/Program.cs
@@ -8,12 +8,23 @@ namespace Day5_Practice_Problem
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the Divisor ");
-            int Divisor = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Dividend ");
-            int Dividend = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Quotient is: {Dividend / Divisor}");
-            Console.WriteLine($"Remainder is: {Dividend % Divisor}");
+            int i, n;
+            double s = 0.0;
+
+            Console.Write("\n\n");
+            Console.Write("Calculate the harmonic series and their sum:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
         }
 
     }
